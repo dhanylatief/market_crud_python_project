35050e330508f517e8c9ab6f9c6e42da1e4fc87f
Dhany <m.dhany.latief@gmail.com>
2024-10-03 16:20:09 +0700

    Add Payment Validation

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ PriceList = f'''
 Harga Apel = {price_apple}
 Harga Jeruk = {price_orange}
 Harga Anggur = {price_grape}'''
-print(PriceList)
 #Input
 amount_apple = input("Jumlah apel yg dibeli: ")
 amount_orange = input("Jumlah jeruk yg dibeli: ")
@@ -19,8 +18,22 @@ amount_grape = int(amount_grape)
 price_apple_total = price_apple*amount_apple
 price_orange_total = price_orange*amount_orange
 price_grape_total = price_grape*amount_grape
+total_all = price_apple_total+price_orange_total+price_grape_total
 #Detail belanja
 print(f"Apel: {amount_apple} x {price_apple} = {price_apple_total}")
 print(f"Jeruk: {amount_orange} x {price_orange} = {price_orange_total}")
 print(f"Anggur: {amount_grape} x {price_grape} = {price_grape_total}")
-print("Total belanja: ", price_apple_total+price_orange_total+price_grape_total)
+print("Total belanja: ", total_all)
+#Pembayaran
+money = input("Masukkan Jumlah Uang: ")
+money = float(money)
+if money >= 0:
+    if money >= total_all:
+        print("Terima kasih")
+        if money > total_all:
+            print("Uang kembali anda: ", money-total_all)
+    elif money < total_all:
+        print("Transaksi Anda Dibatalkan")
+        print("Uang anda kurang sebesar: ", abs(money-total_all))
+else:
+    print("Input tidak valid")
